e4551c66515a116ab2bb032f01cc3f9d5b7e16bd
sergeGuetta <revelomahefasergegustave@gmail.com>
2023-02-03 10:46:34 +0300

    Update AdminController.java

--- src/main/java/com/example/enchere/controller/AdminController.java
+++ src/main/java/com/example/enchere/controller/AdminController.java
@@ -34,7 +34,18 @@ public class AdminController {
         }
         return data;
     }
+    @GetMapping("/test")
+    private Data Atestw() {
+        Data data = new Data();
+        try {
 
+            Admin ad =new Admin();
+            data.setData("je suis serge et j'aime la glace");
+        } catch (Exception e) {
+            data.setError(e);
+        }
+        return data;
+    }
     // creating a get mapping that retrieves the detail of a specific marque
     @GetMapping("/admin/{admin_id}")
     private Data getAdmin(@PathVariable("admin_id") int admin_id) {
